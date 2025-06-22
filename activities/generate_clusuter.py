79fbd925fbf1fd3bc8d12eed7bf2eec7ae2d17e9
Sam <singbar00@gmail.com>
2025-06-22 09:11:40 -0700

    fix broken imports

--- activities/generate_clusuter.py
+++ activities/generate_clusuter.py
@@ -4,5 +4,5 @@ from utils.job_generator import generate_cluster_state
 from utils.to_builtin import to_builtin
 
 @activity.defn
-async def generate_cluster(debug: bool = False):
+async def generate_cluster_activity(debug: bool = False):
     return to_builtin(generate_cluster_state(debug=debug))
