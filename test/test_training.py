de0de1e46107c1d59688d9cc0ff370419ee08281
Sam <singbar00@gmail.com>
2025-06-22 01:06:18 -0700

    add config to test script

--- test/test_training.py
+++ test/test_training.py
@@ -7,6 +7,13 @@ async def main():
     client = await Client.connect("localhost:7233")  # or cloud endpoint
     result = await client.start_workflow(
         TrainingWorkflow.run,
+        TrainingConfig(
+            iterations = 10,
+            checkpoint_dir = "ppo_training_scheduler_checkpoint",
+            env_name ="env.training_scheduler_env.TrainingJobScheduleEnv",
+            num_gpus=0,
+            train_batch_size = 1000
+        ),
         id="rl-training-test-001",
         task_queue="ml-scheduler-task-queue",
     )
