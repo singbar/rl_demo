ed2ed750ddbef99c73641ddf75a0a60287f2a898
Sam <singbar00@gmail.com>
2025-06-22 13:35:12 -0700

    made the training v2 loop

--- workflows/training_workflow_v2.py
+++ workflows/training_workflow_v2.py
@@ -15,14 +15,29 @@ class TrainingConfig:
 
 @workflow.defn
 class TrainingWorkflowLoop:
+    def __init__(self):
+        self.keep_training = True
+
     @workflow.run
     async def run(self, config: TrainingConfig) -> dict:
-        checkpoint_path = await workflow.execute_activity(
-            train_policy_activity,
-            args=[asdict(config)],
-            start_to_close_timeout=timedelta(minutes=15),
-        )
+        # Register stop signal handler
+        workflow.set_signal_handler("stop_training", self.stop_training)
+
+        i = 0
+        last_checkpoint_path = None
+        while self.keep_training:
+            print(f"Training iteration block {i}")
+            last_checkpoint_path = await workflow.execute_activity(
+                train_policy_activity,
+                args=[asdict(config)],
+                start_to_close_timeout=timedelta(minutes=15),
+            )
+            i += 1
 
         return {
-            "checkpoint_path": checkpoint_path,
+            "stopped_after": i,
+            "checkpoint_path": last_checkpoint_path,
         }
+
+    def stop_training(self):
+        self.keep_training = False
