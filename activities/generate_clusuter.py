002ffee7e5a34998520e4a01d50724dbb7d29a61
Sam <singbar00@gmail.com>
2025-06-22 01:12:26 -0700

    fix positional args

--- activities/generate_clusuter.py
+++ activities/generate_clusuter.py
@@ -2,5 +2,5 @@ from temporalio import activity
 from utils.job_generator import generate_cluster_state
 
 @activity.defn
-async def generate_cluster_activity():
+async def generate_cluster_activity(_: object=None):
     return generate_cluster_state()
--- activities/generate_jobs.py
+++ activities/generate_jobs.py
@@ -3,5 +3,5 @@ from utils.job_generator import generate_pending_jobs
 from utils.to_builtin import to_builtin
 
 @activity.defn
-async def generate_jobs_activity():
+async def generate_jobs_activity(_: object=None):
     return to_builtin(generate_pending_jobs())
